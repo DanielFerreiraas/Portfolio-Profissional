8faa97d40b170520b41dfb2a207d28cd4ac9cab7
Daniel Ferreira <daniel.ferreirass.dev@gmail.com>
2023-04-15 23:17:15 -0300

    corrigindo deploy

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./portfolio.css";
 
-import ItemPortfolio from "../itemPortfolio/itemPortfolio";
+import ItemPortfolio from "../ItemPortfolio/ItemPortfolio";
 
 import portfolio from "./Portfolio.json"
 
